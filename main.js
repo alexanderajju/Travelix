493125cfa6de8f064e8f40603cb301b1185208e8
alexanderajju <alexanderajju@gmail.com>
2020-08-10 10:22:26 +0530

    Nav bar scrolling

--- main.js
+++ main.js
@@ -2,6 +2,11 @@
 let header = document.querySelector(".header");
 let hamburgerMenu = document.querySelector(".hamburger-menu");
 
+window.addEventListener("scroll", function () {
+  let windowPostion = window.scrollY > 0;
+  header.classList.toggle("active", windowPostion);
+});
+
 hamburgerMenu.addEventListener("click", function () {
   header.classList.toggle("nav-open");
 });
